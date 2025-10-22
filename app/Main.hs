e5f4013b3171ef613589b03f2405c7b6066ac997
Akos Fabian <fabian.akos@gmail.com>
2025-10-22 18:54:30 +0200

    q to quit

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,14 @@
 module Main (main) where
 
 data Model = Model
-  { content :: String
+  { content :: String,
+    quit :: Bool
   }
   deriving (Show)
 
 data Msg
   = Inc
+  | Quit
   | Nope
   deriving (Show)
 
@@ -16,6 +18,7 @@ update :: Msg -> Model -> Model
 update msg model =
   case msg of
     Inc -> model {content = model.content ++ "."}
+    Quit -> model {quit = True}
     Nope -> model
 
 render :: Model -> String
@@ -23,19 +26,24 @@ render model = show model
 
 main :: IO ()
 main = do
-  let initialModel = Model {content = ""}
+  let initialModel = Model {content = "", quit = False}
   loop initialModel
 
 loop :: Model -> IO ()
 loop model = do
   putStrLn $ render model
-  putStrLn "Enter 'i' to add dots."
+  putStrLn "'i' to add dots. 'q' to quit."
   line <- getLine
   let msg = lineToMsg line
   let newModel = update msg model
-  loop newModel
+  if newModel.quit == True
+    then
+      putStrLn "Bye!"
+    else
+      loop newModel
 
 lineToMsg :: String -> Msg
 lineToMsg line = case line of
   "i" -> Inc
+  "q" -> Quit
   _ -> Nope
