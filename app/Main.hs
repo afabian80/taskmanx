c863615a552a8f25c1b16fdc881600a310d0fb9d
Akos Fabian <fabian.akos@gmail.com>
2025-10-22 07:14:29 +0200

    working inc dec nope

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,44 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module Main (main) where
 
-type Model = Int
+data Model = Model
+  { value :: Int
+  }
+  deriving (Show)
 
-data Msg = Update deriving (Show)
+data Msg
+  = Inc
+  | Dec
+  | Nope
+  deriving (Show)
 
 update :: Msg -> Model -> Model
 update msg model =
   case msg of
-    Update -> model + 1
+    Inc -> model {value = model.value + 1}
+    Dec -> model {value = model.value - 1}
+    Nope -> model
 
 render :: Model -> String
 render model = show model
 
 main :: IO ()
 main = do
-  let initialModel = 0
+  let initialModel = Model {value = 0}
   loop initialModel
 
 loop :: Model -> IO ()
 loop model = do
   putStrLn $ render model
-  putStrLn "Press Enter"
-  _ <- getLine
-  let newModel = update Update model
+  putStrLn "Enter 'i' for increment, 'd' for decrement."
+  line <- getLine
+  let msg = lineToMsg line
+  let newModel = update msg model
   loop newModel
+
+lineToMsg :: String -> Msg
+lineToMsg line = case line of
+  "i" -> Inc
+  "d" -> Dec
+  _ -> Nope
